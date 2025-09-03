d1c3bd8cda7366f34a7eee3614ef153706117320
vini862 <sneh8361@gmail.com>
2025-09-03 12:55:04 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,15 @@
+<script>
+var sidenav = document.querySelector(".side-navbar")
+
+function shownavbar()
+{
+  sidenav.style.left="0"
+}
+
+
+function closenavbar()
+{
+  sidenav.style.left="-60%"
+
+}
+</script>
